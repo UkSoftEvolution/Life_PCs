a07019594fbad5a27c9af52c8f2f2a576e0bff77
GreshnikAlex <1995BLADE@mail.ru>
2019-03-23 20:51:41 +0200

    - Создание и настройка проекта по разработке DLL для работы с CPU

---
+++ Life PCs.CPU/Processor.cs
@@ -0,0 +1,9 @@
+﻿namespace Life_PCs.CPU
+{
+    /// <summary>
+    /// Класс с методами для работы с CPU компьютера
+    /// </summary>
+    public class Processor
+    {
+    }
+}
---
+++ Life PCs.CPU/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Life PCs.CPU")]
+[assembly: AssemblyDescription("Библиотека для работы с CPU")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Uk. Soft-Evolution")]
+[assembly: AssemblyProduct("Life PCs.CPU")]
+[assembly: AssemblyCopyright("Copyright © Uk. Soft-Evolution 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("40333eee-309b-48db-bb83-3f71d6543901")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
