4e1394532a7cea12c05491c087fc9ea1c3fb305f
GreshnikAlex <1995BLADE@mail.ru>
2019-03-24 18:59:59 +0200

    - Подготовка к реализации работы с локальной памятью

---
+++ Life PCs/Life PCs/View/SsdHddView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Life_PCs.View
+{
+    /// <summary>
+    /// Логика взаимодействия для SsdHddView.xaml
+    /// </summary>
+    public partial class SsdHddView : Page
+    {
+        /// <summary>
+        /// Стандартный конструктор
+        /// </summary>
+        public SsdHddView() => InitializeComponent();
+    }
+}
---
+++ Life PCs/Life PCs/ViewModel/SsdHddViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Life_PCs.Other;
+
+namespace Life_PCs.ViewModel
+{
+    /// <summary>
+    /// ViewModel: Класс для Binding с SsdHddView
+    /// </summary>
+    public class SsdHddViewModel : MVVM
+    {
+    }
+}
