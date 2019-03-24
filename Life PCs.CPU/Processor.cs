37944b78bc50d70d2c6bb34bdde43c0b031f25ee
GreshnikAlex <1995BLADE@mail.ru>
2019-03-24 18:43:49 +0200

    - Реализация DLL для работы с CPU

--- Life PCs.CPU/Processor.cs
+++ Life PCs.CPU/Processor.cs
@@ -1,9 +1,27 @@
-﻿namespace Life_PCs.CPU
+﻿using System;
+using System.Management;
+
+namespace Life_PCs.CPU
 {
     /// <summary>
     /// Класс с методами для работы с CPU компьютера
     /// </summary>
     public class Processor
     {
+        #region Methods
+        /// <summary>
+        /// Метод для определения нагрузки CPU
+        /// </summary>
+        /// <returns>item1: Имя процессора, item2: Загруженность процессора</returns>
+        public (string, int) Refresh()
+        {
+            var managementObjectSearcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            var Get = managementObjectSearcher.Get();
+            var GetEnumerator = Get.GetEnumerator();
+            GetEnumerator.MoveNext();
+            var managementObject = (ManagementObject)GetEnumerator.Current;
+            return (managementObject["Name"].ToString(), Convert.ToInt16(managementObject["LoadPercentage"]));
+        }
+        #endregion
     }
 }
